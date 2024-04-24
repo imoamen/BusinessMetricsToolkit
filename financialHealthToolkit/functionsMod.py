4365eade8e8e35f4ed8e97b3293c1689992ea175
poetman <125457215+goodMindMan@users.noreply.github.com>
2024-04-24 15:37:21 +0200

    Update functionsMod.py

--- financialHealthToolkit/functionsMod.py
+++ financialHealthToolkit/functionsMod.py
@@ -9,5 +9,4 @@ def multi_period_table(*in_objs):
     for in_obj in in_objs:
         # Concatenate the DataFrame generated from each instance using create_df method
         df_multi_period = pd.concat([df_multi_period, in_obj.create_df()], ignore_index=False)
-    # return concatenated DataFrame
     return df_multi_period
